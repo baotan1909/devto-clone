ebcf5dcbaf8e4e770e2847f24ac12b76d50066db
Tân <baotan1909@gmail.com>
2025-09-08 19:14:04 +0700

    style: update heading text and adjust divider placement on login page

--- src/app/auth/signIn/page.tsx
+++ src/app/auth/signIn/page.tsx
@@ -9,11 +9,11 @@ export default async function SignInPage() {
             <Image
             src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/original_logo_0DliJcfsTcciZen38gX9.png"
             alt="Dev Community Logo"
-            width={80}
-            height={80}
+            width={60}
+            height={48}
             />
             <h1 className="text-2xl font-bold text-gray-900">
-                Join the Dev community
+                Join the DEV community
             </h1>
             <p className="text-sm text-gray-600 max-w-sm">
                 DEV Community is a community of 1 amazing developer
@@ -21,15 +21,6 @@ export default async function SignInPage() {
         </div>
 
         <SigninForm />
-
-        <div className="relative my-6">
-            <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t border-gray-300"></span>
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-white px-2 text-gray-500"></span>
-            </div>
-        </div>
 
         <p className="text-xs text-gray-500 text-center leading-relaxed">
             By signing up, you are agreeing to our{" "}
@@ -39,6 +30,15 @@ export default async function SignInPage() {
             and{" "}
             <span className="underline cursor-pointer hover:text-gray-700">code of conduct</span>.
         </p>
+
+        <div className="relative my-6">
+            <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t border-gray-300"></span>
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-white px-2 text-gray-500"></span>
+            </div>
+        </div>
 
         <p className="text-center text-sm text-gray-700">
             Already have an account?{" "}
