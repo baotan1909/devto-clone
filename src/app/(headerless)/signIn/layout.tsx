6ed5f4a2cba11efcbe4f15209f0b7f10833c50d3
Tân <baotan1909@gmail.com>
2025-09-25 16:16:55 +0700

    refactor: bring back RootLayout for base document structure

--- src/app/(headerless)/signIn/layout.tsx
+++ src/app/(headerless)/signIn/layout.tsx
@@ -1,8 +1,4 @@
-import "~/styles/globals.css";
-
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
-import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
   title: "Welcome! - Dev Community",
@@ -10,23 +6,10 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
-
-export default function SignInLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function SignInLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>
-        <TRPCReactProvider>
-          <div className="flex min-h-screen items-center justify-center bg-white">
-            <div className="w-full max-w-md px-6">{children}</div>
-          </div>
-        </TRPCReactProvider>
-      </body>
-    </html>
+      <div className="flex min-h-screen items-center justify-center bg-white">
+        <div className="w-full max-w-md px-6">{children}</div>
+      </div>
   );
 }
