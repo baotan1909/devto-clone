bfe55b430c3d00bd17e993947726331d55176565
Tân <baotan1909@gmail.com>
2025-10-06 16:05:24 +0700

    feat: redirect unauthenticated users to signIn and show user id in settings

--- src/app/(main)/settings/layout.tsx
+++ src/app/(main)/settings/layout.tsx
@@ -1,6 +1,13 @@
 import MenuItem from "~/app/_components/menuItem";
+import { auth } from "~/server/auth";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function SettingsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    const session = await auth();
+    if (!session?.user) {
+        redirect("/signIn");
+    }
     return (
         <div className="bg-gray-50 w-full min-h-screen flex flex-col">
             <div className="flex justify-center p-4 flex-1">
@@ -17,7 +24,10 @@ export default async function SettingsLayout({ children }: Readonly<{ children: 
                         </ul>
                     </nav>
                     {/* Content */}
-                    <main className="p-6">{children}</main>
+                    <main className="p-6">
+                        <h1 className="text-2xl text-blue-700"><Link href={`/${session?.user.id}`}>@{session?.user.id}</Link></h1>
+                        {children}
+                    </main>
                 </div>
             </div>
         </div>
