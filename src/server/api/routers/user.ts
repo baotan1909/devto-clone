65401868b8488e34e1d9d432b7a1b4ca3a912bf2
Tân <baotan1909@gmail.com>
2025-10-08 14:17:12 +0700

    feat: update user router to support new User model fields

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -11,14 +11,46 @@ export const userRouter = createTRPCRouter({
         .query(async ({ ctx, input }) => {
             return await ctx.db.user.findUnique({
                 where: { id: input.id },
-                select: { id: true, name: true, image: true },
+                select: {
+                    id: true,
+                    name: true,
+                    image: true,
+                    display_email_on_profile: true,
+                    website_url: true,
+                    location: true,
+                    bio: true,
+                    learning: true,
+                    available_for: true,
+                    skills: true,
+                    hacking: true,
+                    pronouns: true,
+                    work: true,
+                    education: true,
+                    brand_color: true,
+                },
             });
     }),
 
     getCurrentUser: protectedProcedure.query(async ({ ctx }) => {
         return await ctx.db.user.findUnique({
             where: { id: ctx.session.user.id },
-            select: { id: true, name: true, image: true },
+            select: {
+                id: true,
+                name: true,
+                image: true,
+                display_email_on_profile: true,
+                website_url: true,
+                location: true,
+                bio: true,
+                learning: true,
+                available_for: true,
+                skills: true,
+                hacking: true,
+                pronouns: true,
+                work: true,
+                education: true,
+                brand_color: true,
+            },
         });
     })
 });
