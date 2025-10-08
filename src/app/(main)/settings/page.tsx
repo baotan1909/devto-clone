75b91403a0bc1f6307ec544e787a3b0e1ce5a162
Tân <baotan1909@gmail.com>
2025-10-08 13:35:27 +0700

    feat: set default page of settings to profile settings

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,9 +1,6 @@
+import Profile from "./profile/page"
 export default function Settings() {
     return(
-        <div className="flex flex-col h-full space-y-4">
-            <div className="flex-1 w-full bg-black text-white">Test 1</div>
-            <div className="flex-1 w-full bg-black text-white">Test 2</div>
-            <div className="flex-1 w-full bg-black text-white">Test 3</div>
-        </div>
+        <Profile/>
     )
 }
